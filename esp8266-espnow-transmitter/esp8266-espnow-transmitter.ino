df42c0666d69cc5ff0a07bd1a9b6f6ec8ef5ad0e
Lucas Magalhaes <lucas.magalhaes1985@gmail.com>
2020-10-23 08:53:41 -0300

    c

--- esp8266-espnow-transmitter/esp8266-espnow-transmitter.ino
+++ esp8266-espnow-transmitter/esp8266-espnow-transmitter.ino
@@ -5,48 +5,48 @@
 // Then, select the board "NodeMCU 1.0: ESP12-E"
 // ESP-NOW for ESP32 has different API from the ESP8266 implementation
 
-#include <ESP8266WiFi.h>
+#include <WiFi.h>
 #include <espnow.h>
 
 #define RETRY_INTERVAL 5000
 #define SEND_INTERVAL 1000 
 
-// Use the following pattern to create a Locally Administered MAC Address
-//   x2-xx-xx-xx-xx-xx
-//   x6-xx-xx-xx-xx-xx
-//   xA-xx-xx-xx-xx-xx
-//   xE-xx-xx-xx-xx-xx
-// replace x with any hex value
+//// Use the following pattern to create a Locally Administered MAC Address
+////   x2-xx-xx-xx-xx-xx
+////   x6-xx-xx-xx-xx-xx
+////   xA-xx-xx-xx-xx-xx
+////   xE-xx-xx-xx-xx-xx
+//// replace x with any hex value
+//
+//// the following three settings must match the slave settings
+//uint8_t remoteMac[] = {0x82, 0x88, 0x88, 0x88, 0x88, 0x88};
+//const int channel = 14;
+//struct __attribute__((packed)) DataStruct {
+//  float temperature;
+//  float humidity;
+//};
+//
+//DataStruct myData;
+//
+//unsigned long sentStartTime;
+//unsigned long lastSentTime;
+//
+//void sendData() {
+//  uint8_t bs[sizeof(myData)];
+//  memcpy(bs, &myData, sizeof(myData));
+//  
+//  sentStartTime = micros();
+//  esp_now_send(NULL, bs, sizeof(myData)); // NULL means send to all peers
+//}
 
-// the following three settings must match the slave settings
-uint8_t remoteMac[] = {0x82, 0x88, 0x88, 0x88, 0x88, 0x88};
-const uint8_t channel = 14;
-struct __attribute__((packed)) DataStruct {
-  float temperature;
-  float humidity;
-};
-
-DataStruct myData;
-
-unsigned long sentStartTime;
-unsigned long lastSentTime;
-
-void sendData() {
-  uint8_t bs[sizeof(myData)];
-  memcpy(bs, &myData, sizeof(myData));
-  
-  sentStartTime = micros();
-  esp_now_send(NULL, bs, sizeof(myData)); // NULL means send to all peers
-}
-
-void sendCallBackFunction(const uint8_t* mac, const uint8_t sendStatus) {
-  unsigned long sentEndTime = micros();
-  Serial.printf("Send To: %02x:%02x:%02x:%02x:%02x:%02x ", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-  Serial.printf("Temperature: %.2f ", myData.temperature);
-  Serial.printf("Humidity: %.2f ", myData.humidity);
-  Serial.printf("Trip micros: %4lu, ", sentEndTime - sentStartTime);
-  Serial.printf("Status: %s\n", (sendStatus == 0 ? "Success" : "Failed"));
-}
+//void sendCallBackFunction(const uint8_t* mac, const uint8_t sendStatus) {
+//  unsigned long sentEndTime = micros();
+//  Serial.printf("Send To: %02x:%02x:%02x:%02x:%02x:%02x ", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+//  Serial.printf("Temperature: %.2f ", myData.temperature);
+//  Serial.printf("Humidity: %.2f ", myData.humidity);
+//  Serial.printf("Trip micros: %4lu, ", sentEndTime - sentStartTime);
+//  Serial.printf("Status: %s\n", (sendStatus == 0 ? "Success" : "Failed"));
+//}
 
 void setup() {
   WiFi.mode(WIFI_STA); // Station mode for esp-now controller
@@ -77,4 +77,5 @@ void loop() {
 //    myData.humidity = 70.8;      // replace this with your actual sensor reading code
 //    sendData();  
 //  }
+  Serial.printf("WiFi Channel");
 }
