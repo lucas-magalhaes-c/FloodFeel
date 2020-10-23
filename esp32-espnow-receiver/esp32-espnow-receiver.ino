0119b0e56c98723ce4a7ea50c438a3ac941502d9
Lucas Magalhaes <lucas.magalhaes1985@gmail.com>
2020-10-23 14:21:07 -0300

    Update data struct

--- esp32-espnow-receiver/esp32-espnow-receiver.ino
+++ esp32-espnow-receiver/esp32-espnow-receiver.ino
@@ -16,8 +16,8 @@
 uint8_t mac[] = {0x84, 0x0D, 0x8E, 0xA9, 0xB9, 0x40};
 const uint8_t channel = 14;
 struct __attribute__((packed)) DataStruct {
-  float temperature;
-  float humidity;
+  int distance;
+  String transmitter_mac;
 };
 
 DataStruct myData;
@@ -26,20 +26,25 @@ void receiveCallBackFunction(const uint8_t *senderMac, const uint8_t *incomingDa
 //  memcpy(&myData, incomingData, sizeof(myData));
   memcpy(&myData, incomingData, len); 
   
-  Serial.printf("Transmitter MacAddr: %02x:%02x:%02x:%02x:%02x:%02x, ", senderMac[0], senderMac[1], senderMac[2], senderMac[3], senderMac[4], senderMac[5]);
-  Serial.printf("Temperature: %.2f, ", myData.temperature);
-  Serial.printf("Humidity: %.2f\n", myData.humidity);
-
+  Serial.printf("Transmitter MacAddr: %02x:%02x:%02x:%02x:%02x:%02x\n", senderMac[0], senderMac[1], senderMac[2], senderMac[3], senderMac[4], senderMac[5]);
+  
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(myData.distance);
+  // Prints the distance on the Serial Monitor
+  Serial.print("Trasmitter mac adress: ");
+  Serial.println(myData.transmitter_mac);
+  
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(50);
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off (LOW is the voltage level)
 }
 
 void setup() {
+  Serial.begin(115200);
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
   
-  Serial.begin(115200);
   Serial.print("Starting..."); 
   delay(500); 
   
