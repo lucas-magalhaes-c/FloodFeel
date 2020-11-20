45111128ee1e11dfe4444fc0357c694de22f2347
Lucas Magalhaes <lucas.magalhaes1985@gmail.com>
2020-11-19 22:43:54 -0300

    Cloud function to transfer data from Google Firestore to Google Big Query. Adapted from TCC_FloodFeel project to work with sensors data

---
+++ fs_to_bq_cf/main.py
@@ -0,0 +1,71 @@
+from google.api_core.datetime_helpers import DatetimeWithNanoseconds
+from utils import FirestoreHandler, BigQueryHandler
+from datetime import datetime
+import sys
+import json
+
+date = datetime.today().strftime('%Y-%m-%d')
+
+def main (request):
+    config = None
+
+    config = json.loads(open("config_local.json").read()) if "-local" in sys.argv else json.loads(open("config.json").read())
+
+    FH = FirestoreHandler()
+    docs = FH.get_documents(
+        collection='sensors_data'
+    )
+
+    doc_ids_separated_by_date = {}
+    data_separated_by_date = {}
+    schema = []
+    
+    table_date = None
+    
+    for doc in docs:
+        
+        doc_dict = doc.to_dict()
+
+        # extracts date in format YYYY-MM-DD
+        if isinstance(doc_dict["date"], DatetimeWithNanoseconds):
+            table_date = str(doc_dict["date"].year) +"-"+ str(doc_dict["date"].month) +"-"+ str(doc_dict["date"].day) 
+        else: 
+            table_date = doc_dict["date"]
+
+        # create a date on the data list
+        if table_date not in data_separated_by_date:
+            data_separated_by_date[table_date] = []
+            doc_ids_separated_by_date[table_date] = []
+
+        doc_ids_separated_by_date[table_date].append(doc.id)
+
+        # inserts in the Big Query Schema order
+        data_separated_by_date[table_date].append({
+            "date": table_date,
+            "timestamp_ms": doc_dict["timestamp_ms"],
+            "collector_node_id": doc_dict["collector_node_id"],
+            "central_node_id": doc_dict["central_node_id"],
+            "water_level": doc_dict["water_level"],
+            "lat_long": doc_dict["lat_long"]
+        })
+
+    # Checks if it's empty
+    if not bool(data_separated_by_date):
+        print("No data to upload into bq")
+        return
+    
+    # Starts BQ Handler, insert data and checks if data is upload successfuly
+    BQ = BigQueryHandler(config)
+    for date in data_separated_by_date:
+        success = BQ.insert_data(data=data_separated_by_date[date], table_type="sensors_data_fs",table_date=date)
+
+        if success == True:
+            # Update fs_state to sent to bq for all docs
+            FH.set_sent_to_bq_fs_state(collection='sensors_data', doc_ids=doc_ids_separated_by_date[date])
+            print(f"Done. Data from date {date} was sent to Big Query")
+        else:
+            print(f"Failed. Data from date {date} wasn't sent to Big Query")
+    
+    
+if "-local" in sys.argv:
+    main(None)
---
+++ fs_to_bq_cf/utils.py
@@ -0,0 +1,177 @@
+
+import os
+import sys
+import json
+
+from datetime import datetime
+from google.cloud import bigquery
+import google.auth
+
+# https://cloud.google.com/bigquery/docs/loading-data-local
+
+class BigQueryHandler():
+    def __init__(self, config):
+        self.bq_project = config["bq_project"]
+        self.dataset_id =  self.bq_project["dataset_id"]
+        self.table_id_patterns = self.bq_project["table_id_patterns"]
+
+        service_account_path = 'service_account_local.json' if "-local" in sys.argv else 'service_account.json'
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = service_account_path
+
+        # open and load service account & project id
+        sa_file = open(service_account_path)
+        sa_json = json.loads(sa_file.read())
+        self.project_id = sa_json['project_id']
+
+        # Both APIs must be enabled for your project before running this code.
+        # credentials, project = google.auth.default(
+        #         scopes=[
+        #             "https://www.googleapis.com/auth/bigquery",
+        #         ]
+        # )
+
+        # Create a client
+        self.client = bigquery.Client.from_service_account_json(service_account_path)
+        # self.client = bigquery.Client(credentials=credentials, project=project_id)
+    
+    def insert_data(self, data, table_type, table_date):
+
+        dateYYYYMMDD = table_date.replace("-","")
+
+        # Table id in the format TABLE_YYYYMMDD
+        self.table_id = self.table_id_patterns[table_type] + dateYYYYMMDD
+
+        table_full_path = self.project_id+"."+self.dataset_id+"."+self.table_id
+
+        # Check if table already exists
+        try:
+            table_exists = self.client.get_table(table_full_path)
+        except:
+            # Table doesnt exist, lets create it
+
+            schema = self._getSchema(table_type)
+
+            new_table = bigquery.Table(table_full_path, schema=schema)
+            new_table = self.client.create_table(new_table)  # Make an API request.
+        
+        if type(data) is not list: 
+            data_aux = data
+            data = []
+            data.append(data_aux)
+
+        try:
+            status = self.client.insert_rows_json(table=table_full_path, json_rows=data) # Make an API request.
+            if len(status) == 0:
+                return True
+            else:
+                try:
+                    print("One or more errors happened")
+                    for item in status:
+                        errors = item["errors"]
+                        for error in errors:
+                            print(f'Reason: {error["reason"]}, issue: {error["message"]}')
+                except:
+                    print("Failed in showing all the errors")
+                return False
+
+        except Exception as e:
+            print("Failed to insert data into BigQuery\n",e)
+            return False
+    
+    def _getSchema(self,table_type):
+        if table_type == "sensors_data_fs":
+            return [
+                bigquery.SchemaField("date", "DATE", mode="REQUIRED"),
+                bigquery.SchemaField("timestamp_ms", "INTEGER", mode="REQUIRED"),
+                bigquery.SchemaField("collector_node_id", "STRING", mode="REQUIRED"),
+                bigquery.SchemaField("central_node_id", "STRING", mode="REQUIRED"),
+                bigquery.SchemaField("water_level", "INTEGER", mode="REQUIRED"),
+                bigquery.SchemaField("lat_long", "STRING", mode="REQUIRED")
+            ]
+        else:
+            print("Schema not found for the table type")
+            None
+    
+
+
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+class FirestoreHandler():
+    def __init__(self):
+        service_account_path = 'service_account_local.json' if "-local" in sys.argv else 'service_account.json'
+
+        # firestore document state (fs_state)
+        self.fs_state = {
+            1: "Waiting to send to BQ",
+            2: "Sent to BQ",
+        }
+
+        # open and load service account & project id
+        sa_file = open(service_account_path)
+        sa_json = json.loads(sa_file.read())
+        project_id = sa_json['project_id']
+
+        if "-local" not in sys.argv:
+            # Use the application default credentials, in GCP
+            cred = credentials.ApplicationDefault()
+            firebase_admin.initialize_app(cred, {
+            'projectId': project_id,
+            })
+
+            self.db = firestore.client()
+        else:
+            # Testing locally
+            # Use a service account
+            cred = credentials.Certificate(service_account_path)
+            firebase_admin.initialize_app(cred)
+
+            self.db = firestore.client()
+    
+    def add_document_to_collection(self,collection,data,data_type):
+
+        doc_ref = self.db.collection(collection).document(data["collector_node_id"]+":"+ data["timestamp_ms"])
+
+        if data_type == "sensor":
+            doc_ref.set({
+                'date': data["date"],
+                'timestamp_ms': data["timestamp_ms"],
+                'collector_node_id': data["collector_node_id"],
+                'central_node_id': data["central_node_id"],
+                'water_level': data["water_level"],
+                'lat_long': data["lat_long"],
+                'fs_state': 1
+            },merge=True)
+        else:
+            print(f"data_type not recognized {data_type}")
+    
+    def get_documents(self,collection):
+        
+        # Create a reference to the sensors data
+        sensors_data_ref = self.db.collection(collection)
+
+        try:
+            sensors_data_ref = sensors_data_ref.where(u'fs_state', u'==', 1)
+            
+        except Exception as e:
+            print("Failed on getting documents\n",e)
+            return None
+
+        docs = sensors_data_ref.stream()
+
+        # for doc in docs:
+        #     print(f'{doc.id} => {doc.to_dict()}')
+        
+        return docs
+    
+    def set_sent_to_bq_fs_state(self, collection, doc_ids):
+
+        for doc_id in doc_ids:
+            doc_ref = self.db.collection(collection).document(doc_id)
+
+            doc_ref.set({
+                'fs_state': 2
+            },merge=True)
+        
